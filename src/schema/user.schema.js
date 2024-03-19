5e8bfb2c0d39490949e565d64b45ee1d49424c06
ElStarko <114481973+ElStarko@users.noreply.github.com>
2024-03-19 22:40:05 +0100

    Delete src/schema/user.schema.js

--- src/schema/user.schema.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-export const authSchema = Joi.object()
-  .keys({
-    password: Joi.string().min(8).max(30).required(),
-    email: Joi.string().email().required(),
-    role: Joi.string().valid('guest', 'admin')
-  })
-
